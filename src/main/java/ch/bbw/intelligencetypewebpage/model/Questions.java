0f8c2603f4f2b709609c0619f885dcf8dc0e34f9
shan15dev <shansai@bluewin.ch>
2022-06-23 12:39:30 +0200

    Question class added

---
+++ src/main/java/ch/bbw/intelligencetypewebpage/model/Questions.java
@@ -0,0 +1,30 @@
+package ch.bbw.intelligencetypewebpage.model;
+
+import java.util.ArrayList;
+
+public class Questions {
+    private String question;
+    private ArrayList<String> answers = new ArrayList<>();
+
+    public Questions(String question, ArrayList<String> answers) {
+        this.question = question;
+        this.answers = answers;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public ArrayList<String> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(ArrayList<String> answers) {
+        this.answers = answers;
+    }
+
+}
