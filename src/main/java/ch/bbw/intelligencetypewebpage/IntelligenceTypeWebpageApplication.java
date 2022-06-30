98bed971116151ac3a293c916557e4d111cc5ec3
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-06-30 08:57:01 +0200

    Bug fixed

--- src/main/java/ch/bbw/intelligencetypewebpage/IntelligenceTypeWebpageApplication.java
+++ src/main/java/ch/bbw/intelligencetypewebpage/IntelligenceTypeWebpageApplication.java
@@ -1,4 +1,4 @@
-package ch.bbw.intelligencetypewebpage.getstarted;
+package ch.bbw.intelligencetypewebpage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/ch/bbw/intelligencetypewebpage/MainController.java
+++ src/main/java/ch/bbw/intelligencetypewebpage/MainController.java
@@ -1,12 +1,17 @@
 package ch.bbw.intelligencetypewebpage;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import ch.bbw.intelligencetypewebpage.model.Answer;
 import ch.bbw.intelligencetypewebpage.model.Questions;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.ArrayList;
 
+@Controller
 public class MainController {
 
     private ArrayList<Questions> questions = new ArrayList<Questions>();
@@ -14,11 +19,24 @@ public class MainController {
             new Answer(new String[] { "a", "b", "c", "d" }, new String[] { "mathe", "mathe", "mathe", "mathe" }));
 
     public MainController() {
-
+        questions.add(q1);
     }
 
     @GetMapping("")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("index", 0);
+        model.addAttribute("question", questions.get(0));
         return "index";
     }
+
+    @PostMapping("")
+    public String homepost(Model model, @ModelAttribute int index) {
+        if (questions.size() == ++index) { // 1 == ++0 True
+            System.out.println("Exit");
+        }
+        model.addAttribute("index", index);
+        model.addAttribute("question", questions.get(index));
+        return "index";
+    }
+
 }
